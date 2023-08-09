4f9acab736935cfb555cec29946935bf257a8503
Bryan Fernandez <bryan.fernandez.c@upch.pe>
2023-08-08 21:10:44 -0500

    up

---
+++ pointclouds/index/cloud.js
@@ -0,0 +1,26 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "+proj=utm +zone=18 +south +datum=WGS84 +units=m +no_defs",
+    "points": 2865878,
+    "boundingBox": {
+        "lx": 675724.15,
+        "ly": 9572718.5,
+        "lz": 1.11,
+        "ux": 677026.6400000003,
+        "uy": 9574020.99,
+        "uz": 1303.6000000002234
+    },
+    "tightBoundingBox": {
+        "lx": 675724.15,
+        "ly": 9572718.5,
+        "lz": 1.11,
+        "ux": 676932.92,
+        "uy": 9574020.99,
+        "uz": 149.83
+    },
+    "pointAttributes": "LAZ",
+    "spacing": 11.27989387512207,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
